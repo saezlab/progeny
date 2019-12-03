5fe337f1be3f5b8e4135cdfc2a6bec96a90b91c2
Igor Bulanov <bulanov.ig@phystech.edu>
2019-12-03 15:02:52 +0100

    Tests were added

--- tests/testthat/test-progeny_pipeline.R
+++ tests/testthat/test-progeny_pipeline.R
@@ -5,8 +5,8 @@ gene_expr_human = get("gene_expr_human", envir = .GlobalEnv)
 gene_expr_mouse = get("gene_expr_mouse", envir = .GlobalEnv)
 
 #get example output
-result_human_expected <-get("result_human_expected", envir = .GlobalEnv)
-result_mouse_expected <-get("result_mouse_expected", envir = .GlobalEnv)
+result_human_expected = get("result_human_expected", envir = .GlobalEnv)
+result_mouse_expected = get("result_mouse_expected", envir = .GlobalEnv)
 
 #Obtaining actual result
 result_human_actual <- progeny(gene_expr_human, scale=TRUE, 
@@ -19,3 +19,16 @@ test_that("Comparison of the results", {
   expect_equal(result_human_actual, result_human_expected)
   expect_equal(result_mouse_actual, result_mouse_expected)
 })
+
+test_that("Wrong parameters", {
+  expect_error(progeny(gene_expr_human, scale=TRUE, 
+                         organism = "Test", top = 10),
+               "Wrong organism name. Please specify 'Human' or 'Mouse'."
+               )
+  expect_error(progeny(gene_expr_human, scale=TRUE, 
+                       organism = "Human", top = 0),
+               "attempt to set 'rownames' on an object with no dimensions"
+               )
+  
+})
+
