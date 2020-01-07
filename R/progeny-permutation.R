6bd7b8c8b6a7038d80381cc0cd2e37f36105b692
Igor Bulanov <bulanov.ig@phystech.edu>
2020-01-07 15:08:14 +0100

    Edited code width for 80 ch.

--- R/progeny-permutation.R
+++ R/progeny-permutation.R
@@ -1,16 +1,36 @@
-#'This function is designed to compute progeny pathway scores and assess there significance using a gene sampling based permutation strategy, for a series of experimental samples/contrasts.
+#'\code{progeny_perm}
+#'This function is designed to compute progeny pathway scores and assess there 
+#'significance using a gene sampling based permutation strategy, for a series of
+#'experimental samples/contrasts.
 #'
-#'@param df A data.frame of n*m+1 dimension, where n is the number of omic features to be considered and m is the number of samples/contrasts.
-#'The first column should be the identifiers of the omic features. These identifiers must be coherent with the identifers of the weight matrix.
-#'@param weight_matrix A progeny coeficient matrix. the first column should be the identifiers of the omic features, and should be coherent with the identifiers provided in df.
-#'@param k The number of permutations to be preformed to generate the null-distribution used to estimate significance of progeny scores. Default value is 10000.
-#'@param z_score if true, the z-scores will be returned for the pathway activity estimations. Else, the function returns a normalised z-score value between -1 and 1.
-#'@param get_nulldist if true, the null score distribution used for normalisation will be returned along with the actual normalised score data frame.
+#'@param df A data.frame of n*m+1 dimension, where n is the number of omic
+#'features to be considered and m is the number of samples/contrasts.
+#'The first column should be the identifiers of the omic features. 
+#'These identifiers must be coherent with the identifers of the weight matrix.
+#'@param weight_matrix A progeny coeficient matrix. the first column should be
+#'the identifiers of the omic features, and should be coherent with
+#'the identifiers provided in df.
+#'@param k The number of permutations to be preformed to generate 
+#'the null-distribution used to estimate significance of progeny scores. 
+#'Default value is 10000.
+#'@param z_score if true, the z-scores will be returned for 
+#'the pathway activity estimations. Else, the function returns 
+#'a normalised z-score value between -1 and 1.
+#'@param get_nulldist if true, the null score distribution used for normalisation
+#'will be returned along with the actual normalised score data frame.
 #'@export
-#'@return This function returns a list of two elements. The first element is a dataframe of p*m+1 dimensions, where p is the number of progeny pathways, and m is the number of samples/contrasts.
-#'Each cell represent the significance of a progeny pathway score for one sample/contrast. The signifcance ranges between -1 and 1. The significance is equal to x*2-1, x being the quantile of the progeny pathway score with respect to the null distribution.
-#'Thus, this significance can be interpreted as the equivalent of 1-p.value (two sided test over an empirical distribution) with the sign indicating the direction of the regulation.
-#'The second element is the null distribution list (a null distribution is generated for each sample/contrast).
+#'@return This function returns a list of two elements. The first element is 
+#'a dataframe of p*m+1 dimensions, where p is the number of progeny pathways, 
+#'and m is the number of samples/contrasts.
+#'Each cell represent the significance of a progeny pathway score for 
+#'one sample/contrast. The signifcance ranges between -1 and 1. 
+#'The significance is equal to x*2-1, x being the quantile of the progeny
+#'pathway score with respect to the null distribution.
+#'Thus, this significance can be interpreted as the equivalent of 1-p.value
+#'(two sided test over an empirical distribution) with the sign indicating
+#'the direction of the regulation.
+#'The second element is the null distribution list (a null distribution is
+#'generated for each sample/contrast).
 
 progeny_perm <- function(df,weight_matrix,k = 10000, z_scores = T, get_nulldist = F)
 {
@@ -35,7 +55,7 @@ progeny_perm <- function(df,weight_matrix,k = 10000, z_scores = T, get_nulldist 
     common_ids <- common_ids$ID
     common_ids <- as.character(common_ids)
     
-    row.names(current_df) <- current_df$ID
+    row.names(current_df) <- current_df$ID 
     current_df <- as.data.frame(current_df[common_ids,-1])
     
     row.names(current_weights) <- current_weights$ID
@@ -46,7 +66,8 @@ progeny_perm <- function(df,weight_matrix,k = 10000, z_scores = T, get_nulldist 
     
     scores <- as.data.frame(current_weights %*% current_mat)
     
-    null_dist_t <- replicate(k, sample(t_values,length(current_mat[,1]), replace = F))
+    null_dist_t <- replicate(k, sample(t_values,length(current_mat[,1]), 
+                                       replace = F))
     
     null_dist_scores <- current_weights %*% null_dist_t
     
